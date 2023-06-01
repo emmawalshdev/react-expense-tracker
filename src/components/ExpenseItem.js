fd713b0bf0f679d5bc077373d8e274e593d880d5
Emma Walsh <emmaharte33@outlook.com>
2023-06-01 12:02:17 +0100

    Add expensedate component and basic styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props){ //properties
 
+    // pass date props to Expensedate
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <div><ExpenseDate date={props.date}/></div> 
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
