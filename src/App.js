35253564809765dd01a1fe55b1804ca5e76fae08
Emma Walsh <emmaharte33@outlook.com>
2023-06-11 11:59:55 +0100

    Add newexpense and expenseform components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -29,8 +30,8 @@ const App = () => {
   // set
   return (
     <div>
-      <h2>Let's get started!</h2>
-        <Expenses items={expenses}/> 
+      <NewExpense/>
+      <Expenses items={expenses}/> 
     </div> // pass object to expenses */}
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,25 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card.js'
-import React from 'react';
+import React, {useState} from 'react';
 
 const ExpenseItem = (props) => { //properties of app.js
     // render display of each row, use custom component card
+    const [title, setTitle] = useState(props.title); // [managed value, update function]
+
+    const clickHandler = () => {
+        setTitle('Updated');
+    }
     return (
         <Card className='expense-item'> 
             <div><ExpenseDate date={props.date}/></div> 
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>
+                Update Title
+            </button>
         </Card>
     );
 }
