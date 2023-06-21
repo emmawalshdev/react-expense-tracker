b6a54399d721826aea9b9081ee37b18cfddec2c4
Emma Walsh <emmaharte33@outlook.com>
2023-06-21 22:50:44 +0100

    Add condition to dynamically output expenses

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,18 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card.js'
-import React, {useState} from 'react';
+import React from 'react';
 
 const ExpenseItem = (props) => { //properties of app.js
     // render display of each row, use custom component card
-    const [title, setTitle] = useState(props.title); // [managed value, update function]
 
-    const clickHandler = () => {
-        setTitle('Updated');
-    }
     return (
         <Card className='expense-item'> 
             <div><ExpenseDate date={props.date}/></div> 
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>
-                Update Title
-            </button>
         </Card>
     );
 }
